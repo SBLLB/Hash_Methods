c4f9268276b06dbd55cca16a444fd2bdac34b0eb
RachelNolan <mail@rachelnolan.com>
2014-10-03 07:34:59 +0100

    Printing/separating elements of the hash and added README

---
+++ lib/hash_methods.rb
@@ -0,0 +1,9 @@
+test_hash = {:d=>4, :e=>5, :f=>6, :g=>7}
+
+puts "Original hash #{test_hash}"
+#Printing elements of a test_hash
+
+puts "1. Printing the keys to an array #{test_hash.keys.to_a}"
+
+puts "2. Printing the values to an array #{test_hash.values.to_a}"
+
